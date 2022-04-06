30f0ac91b787f5bf72c06ba7d279e16d7e3c85d3
SileboxUnderfined <valerkafm@gmail.com>
2022-04-06 21:39:06 +0300

    Update 6000.js
    -Изменён вызов setTimeout на функцию

--- pagescripts/6000.js
+++ pagescripts/6000.js
@@ -1,1 +1,3 @@
-setTimeout('location="https://www.mspaintadventures.ru/?s=6&p=006001"',2000);
+setTimeout(function(){
+    document.location.href = "https://www.mspaintadventures.ru/?s=6&p=006001";
+},15000);
